a3f8133693d539741230c8b6cc409b93dc4b1659
main <asmail790@gmail.com>
2025-10-22 15:16:19 +0200

    update

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -31,6 +31,10 @@ const CONFIG = {
           'Asmail790/unblock_me_solver_on_android',
           'Asmail790/IconGenerator',
           'Asmail790/house-price-prediction',
+          'Asmail790/Calculator',
+          'Asmail790/DjangoRestNoteEditor',
+          'Asmail790/DjangoNoteEditor',
+          'Asmail790/Simple-Android-Note-Editor-App',
         ], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
@@ -38,22 +42,62 @@ const CONFIG = {
       header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
       projects: [
-        // {
-        //   title: 'Project Name',
-        //   description:
-        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-        //   imageUrl:
-        //     'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-        //   link: 'https://example.com',
-        // },
-        // {
-        //   title: 'Project Name',
-        //   description:
-        //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-        //   imageUrl:
-        //     'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-        //   link: 'https://example.com',
-        // },
+        {
+          title: 'Word segmenter',
+          description: `A word segmenter that segments words such as "don't"  into "do", "not" and "qu'environ"
+            into "qu" and "environ". Tools used are Sklearn, Keras, Numpy and Matplotlib`,
+          imageUrl: '',
+          link: '',
+        },
+        {
+          title: 'Surveillance system',
+          description: `A surveillance system that monitors the front door. 
+          If the door is opened, the system sends a push notification to a phone app and immediately begins recording a live video stream.
+          The live stream is also processed in real-time for face detection.
+          Within the phone application, users can view the live stream, jump back in time, or navigate directly to specific face detection timestamps. 
+          Previously recorded live streams are also accessible for viewing in the app.
+          
+          Currently the following hardware is used to detect the door's state (open/closed) and record the video stream:
+          Raspberry Pi Zero W2 device,camera module,magnetic sensor,Raspberry Pi 4 and PC.
+
+          Currently the languages used in the project include Python, TypeScript, Kotlin, and C. Tools utilized are 
+          Node.js, OpenCV, Linux, Nfty, Mediamtx, Android, and FFmpeg.
+          The project is ongoing.
+          `,
+          imageUrl: '',
+          link: '',
+        },
+        {
+          title: 'Self hosted services',
+          description: `A home server configured for self-hosting services.
+          Currently a Seafile (a file-sharing server) and Forgejo (a git service, ie. a platform for managing software projects) is serviced.
+          Docker Compose is used for containerization and Nginx serves as a TCP proxy. Other tools used are Linux and Wireshark.
+          `,
+          imageUrl: '',
+          link: '',
+        },
+        {
+          title: 'Document management App',
+          description: `An app to streamline the handling of work environment
+          documents. The app retrieves public data from "Arbetsmiljöverket" and "Allabolag.se". Collected data can be exported as CSV or stored in an internal
+          database. The app can also extract information from specific PDF document types
+          from the Arbetsmiljöverket. The information about why Arbetsmiljöverket is inspecting a company and what
+          deficiencies have been identified according to Arbetsmiljöverket  is also summarized from the PDF to provide a clearer overview. The app has also functions for sorting, editing and filtering
+          documents in the database. The app is built upon TypeScript, React and Electron. Other tools used are sqlite,React, Vitest, OpenAI, 
+          Mantine, Kysely,sqlite FTS5, Electron, zod, cheerio, pdf2json, jotai, React Query, CircleCI CI/CD.
+          `,
+          imageUrl: '',
+          link: 'https://drive.google.com/drive/folders/1MC9SP8L_2auuRJSAtU7zAUISaTk3AyQn?usp=drive_link',
+        },
+        {
+          title: 'House sale price analysis',
+          description: `A data analysis predicting sales house prices (regression) was performed using features such as built date, heating, total rooms, total above ground area, and total lot area.
+The analysis also included outlier detection and imputation. 
+The following tools were used: pandas, scipy, sklearn, mlxtend, matplotlib, and seaborn.
+          `,
+          imageUrl: '',
+          link: 'https://www.kaggle.com/code/asmailabdulkarim/house-prices-final',
+        },
       ],
     },
   },
@@ -121,6 +165,16 @@ const CONFIG = {
       'Ultralytics',
       'Tailwind',
       'Django',
+      'Electron',
+      'Zod',
+      'Cheerio',
+      'Kysely',
+      'Puppeter',
+      'Storybook',
+      'Sqlite FTS5',
+      'PIL',
+      'CProfile',
+      'Networkx',
     ].toSorted(),
   ],
   experiences: [
